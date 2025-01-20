b781acd6a65f2e940443ed322a2f4e303153d0de
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-01-20 14:10:33 +0100

    add: inputs with buttons

---
+++ main.js
@@ -0,0 +1,43 @@
+
+const prezzoViaggioPerChilometri = 0.21
+let bigliettoMinorenni = 0.80;
+let bigliettoOver = 0.60;
+let prezzo;
+
+let name = document.getElementById("nome");
+let chilometri = document.getElementById("km");
+let età = document.getElementById("age");
+const btn = document.getElementById("bottone");
+
+
+
+
+btn.addEventListener("click", function(){
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(età < 18 ){
+//      prezzo = ( prezzoViaggioPerChilometri * chilometri ) * bigliettoMinorenni;  
+// } else if (età > 65 ){
+//      prezzo = ( prezzoViaggioPerChilometri * chilometri) * bigliettoOver ;
+
+// } else{
+//      prezzo = (prezzoViaggioPerChilometri * chilometri );
+    
+// }
+
+// console.log( `il prezzo del biglietto è: ${prezzo.toFixed(2)}` )
