bfb244b5498882e34d6f633488e35f0d5bc11328
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-01-20 14:41:44 +0100

    add: first part of js

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-
 const prezzoViaggioPerChilometri = 0.21
 let bigliettoMinorenni = 0.80;
 let bigliettoOver = 0.60;
 let prezzo;
 
-let name = document.getElementById("nome");
-let chilometri = document.getElementById("km");
-let età = document.getElementById("age");
+let nameField = document.getElementById("name-field");
+let kmField = document.getElementById("km-field");
+let ageField = document.getElementById("age-field");
 const btn = document.getElementById("bottone");
 
 
 
+const nome = document.getElementById("name");
+const offer = document.getElementById("offer");
+const carrozza = document.getElementById("carrozza");
+const code = document.getElementById("code");
+const  cost = document.getElementById("cost");
+
+
+if(ageField === "minorenne" ){
+     prezzo = ( prezzoViaggioPerChilometri * kmField ) * bigliettoMinorenni;  
+} else if (ageField === "olre la maggiore età" ){
+     prezzo = ( prezzoViaggioPerChilometri * kmField) * bigliettoOver ;
+
+} else{
+     prezzo = (prezzoViaggioPerChilometri * kmField );
+    
+}
+
+console.log( `il prezzo del biglietto è: ${prezzo.toFixed(2)}` )
 
 btn.addEventListener("click", function(){
-    
+     nome.innerHTML = nameField.value
+     code.innerHTML = Math.floor((Math.random() * 100000) + 1);
+     carrozza.innerHTML = Math.floor((Math.random() * 10) + 1);
+     cost.innerHTML = prezzo.value;
 })
 
 
@@ -30,14 +50,3 @@ btn.addEventListener("click", function(){
 
 
 
-// if(età < 18 ){
-//      prezzo = ( prezzoViaggioPerChilometri * chilometri ) * bigliettoMinorenni;  
-// } else if (età > 65 ){
-//      prezzo = ( prezzoViaggioPerChilometri * chilometri) * bigliettoOver ;
-
-// } else{
-//      prezzo = (prezzoViaggioPerChilometri * chilometri );
-    
-// }
-
-// console.log( `il prezzo del biglietto è: ${prezzo.toFixed(2)}` )
